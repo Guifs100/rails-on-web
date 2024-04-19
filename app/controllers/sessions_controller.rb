c589c3716ff7cbdc4043bfcd428c814ab06f688e
guifs100 <guifs100@gmail.com>
2024-04-19 12:20:13 -0300

    Remove password aleatório gerando ao fazer login com o google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,6 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_by(uid: request.env['omniauth.auth']['uid'], provider: request.env['omniauth.auth']['provider']) do |u|
         u.username = request.env['omniauth.auth']['info']['name']
         u.email = request.env['omniauth.auth']['info']['email']
-        u.password = SecureRandom.hex(10)
     end
     if @user.valid?
         session[:user_id] = @user.id
