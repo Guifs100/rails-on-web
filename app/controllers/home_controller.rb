719dec7ea122fcb77d828741c9e137361c4711f4
guifs100 <guifs100@gmail.com>
2024-04-29 23:03:51 -0300

    Adiciona JWT na aplicação rails

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,51 @@
 class HomeController < ApplicationController
   def index
     required_login
+
+    policies(request_graphql)
+  end
+
+  private
+
+  attr_reader :policies
+
+  def policies(response_body)
+    @policies ||= response_body.dig(:data, :policies)
+  end
+
+  def request_graphql
+    url = URI("http://web:3003/graphql")
+    headers = {
+      'Content-Type' => 'application/json',
+      'Authorization' => "Bearer #{encode}"
+    }
+    body = { query: body_builder }.to_json
+
+    response = Net::HTTP.post(url, body, headers)
+    responsed_body = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def body_builder
+    <<-GRAPHQL
+      query {
+        policies(limit: null) {
+            id
+            dateIssue
+            policyExpiration
+            insured {
+                id
+                name
+                cpf
+            }
+            vehicle {
+                id
+                licensePlate
+                year
+                model
+                brand
+            }
+        }
+      }
+    GRAPHQL
   end
 end
